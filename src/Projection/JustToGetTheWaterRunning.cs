d5d25db5009e42ac90834aeb779df3e510f601c9
David Basarab <dbasarab617@gmail.com>
2021-11-17 22:23:19 -0500

    trying go figure out how I want the entry point to lool

--- src/Projection/JustToGetTheWaterRunning.cs
+++ src/Projection/JustToGetTheWaterRunning.cs
@@ -3,5 +3,26 @@ namespace FatCat.Projection
 	public class JustToGetTheWaterRunning
 	{
 		public int Sum(int number1, int number2) => number1 + number2;
+
+		/*
+		 *
+		 * var someObject = new();
+		 *
+		 * OtherClass projection = Projection<OtherClass>(someObject);
+		 *
+		 *  OtherClass projection = Do.Projection<OtherClass>(someObject);
+		 *  OtherClass projection = A.Projection<OtherClass>(someObject);
+		 *
+		 * OtherClass projection = someObject.Projection<OtherClass>();
+		 *
+		 * * var projection = someObject.ProjectTo<OtherClass>();
+		 *
+		 *
+		 */
+	}
+
+	public static class Projection
+	{
+
 	}
 }

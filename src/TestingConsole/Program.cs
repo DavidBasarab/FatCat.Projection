f232146489ea5f74332cecb8e938b883df074f3b
David Basarab <dbasarab617@gmail.com>
2021-11-17 22:05:27 -0500

    basic project setup

---
+++ src/TestingConsole/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FatCat.Projection.TestingConsole
+{
+	public class Program
+	{
+		public static void Main(params string[] args)
+		{
+			Console.WriteLine("Projection Test Console");
+		}
+	}
+}

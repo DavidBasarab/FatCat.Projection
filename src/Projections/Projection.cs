4ef04197899d457b38934bec7ff86fa8fd991953
David Basarab <dbasarab617@gmail.com>
2021-11-18 22:18:31 -0500

    can do sub object projection

--- src/Projections/Projection.cs
+++ src/Projections/Projection.cs
@@ -1,8 +1,26 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FatCat.Projections
 {
+	internal static class TypeExtensions
+	{
+		private static bool Implements(this Type type, Type interfaceType)
+		{
+			if (type == interfaceType) return false;
+
+			return interfaceType.IsGenericTypeDefinition && type.GetInterfaces().Where(t => t.IsGenericType).Select(t => t.GetGenericTypeDefinition()).Any(gt => gt == interfaceType) || interfaceType.IsAssignableFrom(type);
+		}
+
+		public static bool IsDictionary(this Type type) => type.IsGenericType && type.Implements(typeof(IDictionary<,>));
+
+		public static bool IsList(this Type type) => type.IsGenericType && type.Implements(typeof(IEnumerable));
+
+		public static bool IsNotAList(this Type type) => !type.IsList();
+	}
+
 	public static class Projection
 	{
 		public static TDestination ProjectTo<TDestination>(object source) where TDestination : class
@@ -27,7 +45,15 @@ namespace FatCat.Projections
 
 				if (destinationProperty == null) continue;
 
-				destinationProperty?.SetValue(instance, sourceProperty.GetValue(source));
+				var typeCode = Type.GetTypeCode(destinationProperty.PropertyType);
+
+				if (destinationProperty.PropertyType.IsNotAList() && typeCode == TypeCode.Object)
+				{
+					var propertyValue = ProjectTo(destinationProperty.PropertyType, sourceProperty.GetValue(source)!);
+
+					destinationProperty.SetValue(instance, propertyValue);
+				}
+				else destinationProperty?.SetValue(instance, sourceProperty.GetValue(source));
 			}
 
 			return instance!;

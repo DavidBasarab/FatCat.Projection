47c599b6acf8a9ec65885fa3195a42457b9e5a0f
David Basarab <dbasarab617@gmail.com>
2022-04-23 20:09:30 -0400

    test for list to list projection

---
+++ src/Projections.Tests/ListToListProjections.cs
@@ -0,0 +1,25 @@
+using FatCat.Fakes;
+using FatCat.Projections.Tests.Objects.SimpleItems.ItemsWithLists;
+using FluentAssertions;
+using Xunit;
+
+namespace FatCat.Projections.Tests;
+
+public class ListToListProjections
+{
+	[Fact]
+	public void CanProjectFromListOfSameType()
+	{
+		var sourceList = Faker.Create<List<SourceItemsWithStuff>>();
+
+		var resultList = Projection.ProjectTo<List<SourceItemsWithStuff>>(sourceList);
+
+		resultList
+			.Should()
+			.NotBeNull();
+
+		resultList
+			.Should()
+			.BeEquivalentTo(sourceList);
+	}
+}

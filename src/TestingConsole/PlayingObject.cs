eba3d5b1fb2ee4dd8b8c48d807b789f6ca1e818c
David Basarab <dbasarab617@gmail.com>
2022-04-24 16:56:17 -0400

    on my way to the code I want, but not working yet

--- src/TestingConsole/PlayingObject.cs
+++ src/TestingConsole/PlayingObject.cs
@@ -8,3 +8,14 @@ public class PlayingObject
 
 	public DateTime UpdatedDate { get; set; }
 }
+
+public class PlayingDestination
+{
+	public DateTime CreatedDate { get; set; }
+
+	public string? Name { get; set; }
+
+	public int Number { get; set; }
+
+	public DateTime UpdatedDate { get; set; }
+}
--- src/TestingConsole/Program.cs
+++ src/TestingConsole/Program.cs
@@ -1,26 +1,84 @@
+using System.Linq.Expressions;
 using FatCat.Fakes;
-using FatCat.Projections;
 using FatCat.Toolkit.Console;
-using Newtonsoft.Json;
-
-for (var i = 0; i < 4; i++) ConsoleLog.WriteEmptyLine();
 
 ConsoleLog.Write("Projection Test Console");
 
 try
 {
-	var sourceList = Faker.Create<List<PlayingObject>>();
+	var sourceItem = Faker.Create<PlayingObject>();
 
-	var copyList = ListCopy.Copy(sourceList);
-
-	var referenceEquals = ReferenceEquals(sourceList, copyList);
-
-	ConsoleLog.WriteCyan($"Does the list reference equal | <{referenceEquals}>");
-
-	var json = JsonConvert.SerializeObject(copyList, Formatting.Indented);
-
-	ConsoleLog.WriteMagenta(json);
-
-	var strongList = copyList as List<PlayingObject>;
+	new PlayingProjection<PlayingDestination, PlayingObject>()
+		.ForProperty(i => i.Number, s => 17)
+		.Project(sourceItem);
 }
 catch (Exception e) { ConsoleLog.WriteException(e); }
+
+public class PlayingProjection<TDest, TSource>
+{
+	private Dictionary<string, Func<TSource, object>> overrides = new Dictionary<string, Func<TSource, object>>();
+
+	public PlayingProjection<TDest, TSource> ForProperty<TMember>(Expression<Func<TDest, TMember>> selector, Func<TSource, TMember> memberOptions)
+	{
+		var propertyName = GetMemberName(selector.Body);
+
+		var value = memberOptions(default!);
+		
+		ConsoleLog.WriteMagenta($"So the Property NAME is!!!!!! <{propertyName}> | Value is <{value}>");
+
+		// overrides.Add(propertyName, memberOptions);
+		
+		return this;
+	}
+
+	public TDest Project(object source)
+	{
+		var destinationInstance = Activator.CreateInstance<TDest>();
+
+		ConsoleLog.WriteMagenta($"This is where it would project {typeof(TDest).FullName} from <{source.GetType().FullName}>");
+
+		return default!;
+	}
+	
+	private static string GetMemberName(Expression expression)
+	{
+		if (expression == null)
+		{
+			throw new ArgumentException("expressionCannotBeNullMessage");
+		}
+
+		if (expression is MemberExpression)
+		{
+			// Reference type property or field
+			var memberExpression = (MemberExpression) expression;
+			return memberExpression.Member.Name;
+		}
+
+		if (expression is MethodCallExpression)
+		{
+			// Reference type method
+			var methodCallExpression = (MethodCallExpression) expression;
+			return methodCallExpression.Method.Name;
+		}
+
+		if (expression is UnaryExpression)
+		{
+			// Property, field of method returning value type
+			var unaryExpression = (UnaryExpression) expression;
+			return GetMemberName(unaryExpression);
+		}
+
+		throw new ArgumentException("invalidExpressionMessage");
+	}
+
+	private static string GetMemberName(UnaryExpression unaryExpression)
+	{
+		if (unaryExpression.Operand is MethodCallExpression)
+		{
+			var methodExpression = (MethodCallExpression) unaryExpression.Operand;
+			return methodExpression.Method.Name;
+		}
+
+		return ((MemberExpression) unaryExpression.Operand).Member.Name;
+	}
+}

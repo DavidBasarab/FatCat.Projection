f3ae8035fdd34f52700e3497bb1e40b465cbd589
David Basarab <dbasarab617@gmail.com>
2022-05-01 10:52:53 -0400

    more complex sub

--- src/Projections.Tests/Objects/FluentObjects/FluentComplexSource.cs
+++ src/Projections.Tests/Objects/FluentObjects/FluentComplexSource.cs
@@ -7,9 +7,16 @@ public class FluentComplexSource
 	public string? Title { get; set; }
 }
 
-public class FluentComplexDestination
+public class FluentKindOfSimpleDestination
 {
 	public string? FullTitle { get; set; }
 
 	public int TheNumber { get; set; }
+}
+
+public class FluentComplexDestination
+{
+	public FluentKindOfSimpleDestination SubObject { get; set; }
+
+	public string Name { get; set; }
 }

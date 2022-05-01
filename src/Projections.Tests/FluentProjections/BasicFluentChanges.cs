427f0c814b2471dea836389c43a80c7750a79f9a
David Basarab <dbasarab617@gmail.com>
2022-05-01 10:21:59 -0400

    override from properties

--- src/Projections.Tests/FluentProjections/BasicFluentChanges.cs
+++ src/Projections.Tests/FluentProjections/BasicFluentChanges.cs
@@ -24,4 +24,22 @@ public class BasicFluentChanges
 			.Should()
 			.Be(23);
 	}
+
+	[Fact]
+	public void CanDoOverrideFromObjectProperties()
+	{
+		var source = Faker.Create<FluentSimpleSource>();
+
+		var result = new FluentProjection<FluentSimpleDestination, FluentSimpleSource>()
+					.ForProperty(i => i.DifferentNumber, _ => source.Number)
+					.Project(source);
+
+		result.Name
+			.Should()
+			.Be(source.Name);
+
+		result.DifferentNumber
+			.Should()
+			.Be(source.Number);
+	}
 }

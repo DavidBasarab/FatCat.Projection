9500075c3f2e9842d0be76dafd880b394d6b80a3
David Basarab <dbasarab617@gmail.com>
2023-01-05 21:53:13 -0500

    first test works going to try struct

---
+++ src/Projections.Tests/BasicProjection/ProjectionWithMissingProperty.cs
@@ -0,0 +1,31 @@
+﻿using FatCat.Fakes;
+using Xunit;
+
+namespace FatCat.Projections.Tests.BasicProjection;
+
+public class ProjectionWithMissingProperty
+{
+	[Fact]
+	public void CanProjectWithMissingDestinationFromSource()
+	{
+		var source = Faker.Create<Source>();
+
+		var destination = Projection.ProjectTo<Destination>(source);
+	}
+
+	private class Destination
+	{
+		public string FirstName { get; set; }
+
+		public string Id { get; set; }
+
+		public string LastName { get; set; }
+	}
+
+	private class Source
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+	}
+}

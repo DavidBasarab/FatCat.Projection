1948654db893bb9f02f7513672b8a8d2819ffa92
David Basarab <dbasarab617@gmail.com>
2023-01-09 20:31:35 -0500

    can config customer projections

---
+++ src/Projections.Tests/ProjectUsing/CustomProjectionConfigurationTests.cs
@@ -0,0 +1,52 @@
+﻿using FatCat.Projections.Tests.Objects.SimpleItems;
+using FluentAssertions;
+using Xunit;
+
+namespace FatCat.Projections.Tests.ProjectUsing;
+
+public class CustomProjectionConfigurationTests
+{
+	[Fact]
+	public void CanSaveACustomProjectorToConfig()
+	{
+		ProjectionConfiguration.UseCustomProjection<SimpleDestination, TestProjection>();
+
+		RunGetCustomerProjectorTest<TestProjection>();
+	}
+
+	[Fact]
+	public void TheLastCustomProjectorWillBeUsed()
+	{
+		ProjectionConfiguration.UseCustomProjection<SimpleDestination, TestProjection>();
+		ProjectionConfiguration.UseCustomProjection<SimpleDestination, SecondCustomProjection>();
+
+		RunGetCustomerProjectorTest<SecondCustomProjection>();
+	}
+
+	private static void RunGetCustomerProjectorTest<T>() where T : IDoProjection<SimpleDestination>
+	{
+		var customProjector = ProjectionConfiguration.GetCustomProjector<SimpleDestination>();
+
+		customProjector
+			.Should()
+			.NotBeNull();
+
+		customProjector
+			.Should()
+			.BeOfType<T>();
+	}
+
+	private class SecondCustomProjection : IDoProjection<SimpleDestination>
+	{
+		public void Project(SimpleDestination destinationObject, object source) { throw new NotImplementedException(); }
+
+		public SimpleDestination ProjectTo(object source) => throw new NotImplementedException();
+	}
+
+	private class TestProjection : IDoProjection<SimpleDestination>
+	{
+		public void Project(SimpleDestination destinationObject, object source) { throw new NotImplementedException(); }
+
+		public SimpleDestination ProjectTo(object source) => throw new NotImplementedException();
+	}
+}
---
+++ src/Projections/ProjectionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+
+namespace FatCat.Projections;
+
+public static class ProjectionConfiguration
+{
+	private static ConcurrentDictionary<Type, Type> CustomProjectors { get; } = new();
+
+	public static void UseCustomProjection<TDestination, TCustomProjector>() where TDestination : class where TCustomProjector : IDoProjection<TDestination>
+	{
+		if (CustomProjectors.ContainsKey(typeof(TDestination))) CustomProjectors.TryRemove(typeof(TDestination), out _);
+
+		CustomProjectors.TryAdd(typeof(TDestination), typeof(TCustomProjector));
+	}
+
+	internal static IDoProjection<TDestination>? GetCustomProjector<TDestination>() where TDestination : class
+	{
+		if (CustomProjectors.TryGetValue(typeof(TDestination), out var projectorType)) return Activator.CreateInstance(projectorType) as IDoProjection<TDestination>;
+
+		return null;
+	}
+}

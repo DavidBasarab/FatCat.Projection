c88e58c956f7b836a77d7b375c126860cc529780
David Basarab <dbasarab617@gmail.com>
2023-01-09 20:51:21 -0500

    wip

--- src/Projections.Tests/ProjectUsing/BasicProjectUsingTests.cs
+++ src/Projections.Tests/ProjectUsing/BasicProjectUsingTests.cs
@@ -1,12 +1,65 @@
-﻿namespace FatCat.Projections.Tests.ProjectUsing;
+﻿using FatCat.Fakes;
+using FatCat.Projections.Tests.Objects;
+using FluentAssertions;
+using Xunit;
+
+namespace FatCat.Projections.Tests.ProjectUsing;
 
 public class BasicProjectUsingTests
 {
-	/*
-	 *   How do I want this to work?
-	 *
-	 *
-	 *
-	 * 
-	 */
+	public BasicProjectUsingTests() => ProjectionConfiguration.UseCustomProjection<MultiLevelObjectDestination, TestingCustomProjection>();
+
+	[Fact]
+	public void WillUseCustomProjectionForProjectTo()
+	{
+		var source = Faker.Create<MultiLevelObjectSource>();
+
+		var result = Projection.ProjectTo<MultiLevelObjectDestination>(source);
+
+		result
+			.Should()
+			.Be(TestingCustomProjection.ItemToReturn);
+
+		TestingCustomProjection.WasProjectToCalled
+								.Should()
+								.BeTrue();
+
+		TestingCustomProjection.CalledSource
+								.Should()
+								.Be(source);
+	}
+
+	private class TestingCustomProjection : IDoProjection<MultiLevelObjectDestination>
+	{
+		public static object CalledSource { get; private set; }
+
+		public static MultiLevelObjectDestination ItemToReturn { get; } = Faker.Create<MultiLevelObjectDestination>();
+
+		public static bool WasProjectCalled { get; private set; }
+
+		public static bool WasProjectToCalled { get; private set; }
+
+		public static void Reset()
+		{
+			WasProjectToCalled = false;
+			WasProjectCalled = false;
+			CalledSource = null;
+		}
+
+		public void Project(MultiLevelObjectDestination destinationObject, object source)
+		{
+			destinationObject = ItemToReturn;
+
+			WasProjectCalled = true;
+			CalledSource = source;
+		}
+
+		public MultiLevelObjectDestination ProjectTo(object source)
+		{
+			WasProjectToCalled = true;
+			CalledSource = source;
+
+			return ItemToReturn;
+		}
+	}
 }
